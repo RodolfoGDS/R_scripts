0adb79c061d525914c6f29cbad0704d99675aba7
RodolfoGDS <rreisdev@gmail.com>
2022-09-27 15:44:54 -0300

    Update daily_expenses.R

--- daily.expenses/daily_expenses.R
+++ daily.expenses/daily_expenses.R
@@ -14,7 +14,7 @@ saveData <- function(data, SHEET_ID){
 }
 
 
-# Passando o acesso a ao googlsheet e o endereço para as tabelas
+# Passando o acesso ao googlsheet e o endereço para as tabelas
 
 gs4_auth()
 
@@ -23,9 +23,12 @@ gastos_debito <- "https://docs.google.com/spreadsheets/d/12wxypt8tsiAZBVkFvTfB2Y
 gastos_credito <- "https://docs.google.com/spreadsheets/d/1S4kO_MVcgHTTL3XVvrfhGfMFBNK65pjRjc1686FizYE/edit#gid=0"
 
 
-# Pacotes para carregar o web app
+# Pacotes para carregar o web app e os gráficos
 
 library(shiny)
+library(ggplot2)
+library(plotly)
+library(lubridate)
 
 ##########################################################
 
@@ -51,13 +54,13 @@ ui <-  fluidPage( theme = bslib::bs_theme(bootswatch = "solar"),
                                     
                                     column(4,
                                        selectInput("classeInvestimento", label ="Tipo de Investimento",
-                                                  choices = c("CDBs", "Ações","FIIs", "Renda Fixa", "LCI", "LCA")),
+                                                  choices = c("Nenhum","CDBs", "Ações","FIIs", "Renda Fixa", "LCI", "LCA")),
                                        actionButton("salvar", label = "Salvar os Dados", class = "btn btn sucess"))),
                                 fluidRow(
                                     column(6,
-                                           plotOutput("Receita")),
+                                           plotlyOutput("Receita")),
                                     column(6,
-                                           tableOutput("investimento"))
+                                          plotlyOutput("investimento"))
                                 )
                                    
                                    ),
@@ -83,7 +86,7 @@ ui <-  fluidPage( theme = bslib::bs_theme(bootswatch = "solar"),
                               
                               fluidRow(
                                   column(6,
-                                         plotOutput("GastosDebito")),
+                                         plotlyOutput("GastosDebito")),
                                   column(6,
                                          plotOutput("GastosDebitoCategoria"))
                               )
@@ -115,7 +118,7 @@ ui <-  fluidPage( theme = bslib::bs_theme(bootswatch = "solar"),
                                )
                                  )
                   
-      #############################################################################
+      ####1#########################################################################
                   #Fechamento de fluidPage e tabSetPanel
                     
                       ))
@@ -125,16 +128,38 @@ ui <-  fluidPage( theme = bslib::bs_theme(bootswatch = "solar"),
 
 server <-  function(input, output, session){
 
+  thematic::thematic_shiny()  
+  
   #Back-end Receita e Investimentos
   
     data <- reactive({
-      data.frame(input$salario,input$data, input$rendaExtra,input$valorInvestido,input$classeInvestimento )
+      data.frame(input$salario,input$data, input$rendaExtra,input$valorInvestido,input$classeInvestimento,as.Date(lubridate::month(input$data, label=TRUE)))
     })
     
     observeEvent(input$salvar,{
       saveData(data(), entrada_investimento)
       
     })
+    
+  df <- googlesheets4::read_sheet(entrada_investimento)
+  salario_mes <-reactive( aggregate(df$SALARIO,by=list(df$MES), FUN = sum))
+  names(salario_mes()) <- c("Mes", "Salario")
+  
+    output$Receita <-renderPlotly({
+      ggplot(salario_mes,aes(x=Mes, y = Salario))+
+        geom_bar(stat = "identity", fill = "green", fun=sum)+
+        labs(x="", y="", title = "Salário por mês")
+    }) 
+    
+    output$investimento <- renderPlotly({
+      ggplot(googlesheets4::read_sheet(entrada_investimento), 
+             aes(fill = CLASSE_DE_INVESTIMENTO, x=DATA, y = VALOR_INVESTIDO))+
+               geom_bar(position = "stack",stat = "identity")+
+               theme(legend.position = "none")+
+               labs(x = "", y="", title = "Distribuição dos investimentos por mês")
+    })
+      
+
     
     
    #Back-end Gastos no Débito
@@ -145,6 +170,12 @@ server <-  function(input, output, session){
     
     observeEvent(input$salvar1,{
      saveData(data1(), gastos_debito)
+    })
+    
+    output$GastosDebito <-renderPlotly({
+      ggplot(googlesheets4::read_sheet(gastos_debito),aes(x = lubridate::month(DATA_GASTO), y = GASTO))+
+        geom_bar(stat = "identity", fill="green")+
+        labs(x="", y="", title="Gasto por mês")
     })
     
     #Back-end Gastos no Crédito
