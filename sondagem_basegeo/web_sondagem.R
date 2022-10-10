7b418e2bb9bdca90c997ca590068ff9d318f198e
RodolfoGDS <rreisdev@gmail.com>
2022-10-10 15:03:11 -0300

    WEB_SONDAGEM

--- sondagem_basegeo/web_sondagem.R
+++ sondagem_basegeo/web_sondagem.R
@@ -34,20 +34,21 @@ ui <- fluidPage( theme = bslib::bs_theme(bg = "white",fg = "black",   version = 
                      column(4,
                      textInput("cod_ident_furo",label = "Código de identificação do furo"),
                      textInput("cod_projeto",label =  "Código do Projeto de Sondagem"),
-                     textInput("operador_poço",label =  "Operador do Poço"),
-                     textInput("sondador_poço",label =  "Sondador do Poço" )),
+                     textInput("operador_furo",label =  "Operador do Furo"),
+                     textInput("sondador_furo",label =  "Sondador do Furo" )),
                      
                      column(4,
-                     textInput("custodiante_poço",label = "Custodiante do Poço"),
+                     selectInput("origem_dado", "Fonte de origem dos dados", choices = c("DHT-SIAGAS","DHT-RIMAS","DHT-DEGET", "DGM","ANP","Outros")),
+                     textInput("custodiante_furo",label = "Custodiante do Furo"),
                      dateInput("data_inicio", label = "Data de início da Perfuração", language = "pt", format = "dd-mm-yyyy"),
-                     dateInput("data_final", label = "Data final da Perfuração", language = "pt",  format = "dd-mm-yyyy"),
-                     selectInput("estado", label = "Estado",choices = unique(estados$abbrev_state))),
+                     dateInput("data_final", label = "Data final da Perfuração", language = "pt",  format = "dd-mm-yyyy")),
+                    
                      
                      column(4,
                      
-                     selectizeInput("municipio", label = "Municipio",  choices = NULL),
+                     textInput("identificador_herdado", "Código Herdado"),
                      textInput("referencia", label =  "Documento de Referencia"),
-                     textInput("descritor", label =  "Descritor"),
+                     textInput("observacao", label =  "Observação"),
                      actionButton("Salvar",label = "Salvar",class = "btn-lg btn-sucess"))
                      
                      ),
@@ -60,11 +61,15 @@ ui <- fluidPage( theme = bslib::bs_theme(bg = "white",fg = "black",   version = 
             tabPanel("Collar/Posição",
                      column(3,
                      fileInput("file", "GPS", buttonLabel = "Upload"),
+                     dateInput("data_determinacao", label = "Data de Determinação", language = "pt",  format = "dd-mm-yyyy"),
                      numericInput("latitude", label = "Latitude", value = 0),
                      numericInput("longitude", label = "Longitude", value = 0),
                      numericInput("elevacao", label = "Elevação", value = 0),
+                     textInput("datum", label = "Datum"),
                      textInput("metodo_posicionamento", label=  "Método de Posiconamento"),
                      numericInput("precisao_posicional", label = "Precisão Posicional", value = 0),
+                     selectInput("estado", label = "Estado",choices = unique(estados$abbrev_state)),
+                     selectizeInput("municipio", label = "Municipio",  choices = NULL),
                      actionButton("Salvar",label = "Salvar",class = "btn-lg btn-sucess")),
                      
                      column(9,
@@ -100,22 +105,27 @@ ui <- fluidPage( theme = bslib::bs_theme(bg = "white",fg = "black",   version = 
                             radioButtons("tipo_finalidade", label = "Finalidade da sondagem", 
                                          choices = c("Perfuração para água", "Perfuração Estratigrafica",
                                                      "Perfuração para minério", "Perfuração para óleo e gás",
-                                                     "Perfuração geotecnica"), inline =F),
-                            selectInput("id_metodo_perfuração", "Método de Perfuração", 
-                                       choices = c("Trado","Percursão SPT","Rotativa", "Mista", "Geofisica"), multiple = T)),
+                                                     "Perfuração geotecnica", "Outros"), inline =F),
+                            selectInput("alvo", "Alvo - Target", choices = c("Campo de petroleo", "Corpo Mineralizado",
+                                                                             "Aquifero", "Unidade estratigrafica",
+                                                                             "Bacia Sedimentar"," Bacia Hodrográfica", "Outros"))),
+                           
                       
                             column(4,
                             selectInput("id_equipamento_perfuração", "Equipamento de Perfuração", 
                                        choices = c("Equipamento A", "Equipamento B", "Equipamento C", "Equipamento n..."), multiple = T),
+                            selectInput("id_metodo_perfuração", "Método de Perfuração", 
+                                        choices = c("Trado manual","Trado Mecanico","Percursão SPT","Rotativa", "Mista",
+                                                    "Geofisica", "outros"), multiple = T),
                             numericInput("diametro_poco", "Diametro do Poço",min = 0,max = 100, value = 0)),
                      
                             column(4,
                             numericInput("intervalo_inicio", label = "Intervalo de Inicio", value = 0 ),
                             numericInput("intervalo_fim", label = "Intervalo final", value = 0),
-                            actionButton("Salvar",label = "Salvar",class = "btn-lg btn-sucess")))
-                    ),
+                            actionButton("Salvar",label = "Salvar",class = "btn-lg btn-sucess"))
+                    )
 
-
+           ),
 # Fim da Parte Descrição / Detalhe 
 ###############################################################################                          
          
@@ -128,11 +138,14 @@ ui <- fluidPage( theme = bslib::bs_theme(bg = "white",fg = "black",   version = 
                                     fileInput("file", "Dataset", buttonLabel = "Upload")  ),
                              column(4,
                                     numericInput("azimuth", label = "Azimuth", value = 0 , min = 0 , max = 359),
-                                    actionButton("salvar", label = "Salvar", class = "btn-lg btn-sucess")),
+                                   
+                                    numericInput("avanco", "Avanço", min = 0, value = 0),  
+                                    numericInput("recuperacao", "Recuperação", min = 0 , value = 1)),
+                                   
                              column(4,
-                                    numericInput("inclinacao", label = "Inclinação", value = 0 , min = 0 , max = 90)
-                            
-                           )),
+                                    numericInput("inclinacao", label = "Inclinação", value = 0 , min = 0 , max = 90),
+                                    actionButton("salvar", label = "Salvar", class = "btn-lg btn-sucess"))),
+                          
                    
                    fluidRow(
                      column(12, div(style ="height:50px;background-color: gray","Geologia/Geology")),
@@ -151,68 +164,24 @@ ui <- fluidPage( theme = bslib::bs_theme(bg = "white",fg = "black",   version = 
 
 # Fim da Parte Intervalo/Geologia 
 ###############################################################################   
-            
-    
-        tabPanel("Hidro/Amostra água",
-                     fluidRow(
-                       column(12, div(style = "height: 50px; background-color: gray", "Base Hidro")),
-                        column(3,
-                               textInput("cod_ident_ana",label = "Código de Identificação na ANA")),
-                       column(3,
-                              selectInput("id_material_espaco_anular", label = "Material Espaço Anular",
-                                          choices = c("Material A", "Material B", "Material C", "Material N...."))),
-                       column(3,
-                              selectInput("id_material_revestimento", label ="Material de Resvestimento",
-                                          choices = c("Material A", "Material B", "Material C", "Material N....") )),
-                       column(3,
-                              numericInput("entrada_de_agua", label = "Profundidade de entrada de agua", value = 0))
-                     ),
-                     
-                     fluidRow(
-                       column(12, div(style = "height:50px;background-color:gray", "Amostra de Água")),
-                        
-                       column(4,
-                              textInput("identificacao_amostra",label = "Código da amostra"),
-                              dateInput("data_coleta", label = "Data da Coleta in situ", language = "pt",
-                                        format = "dd-mm-yyyy" ),
-                              textInput("coletor", label = "Coletor")),
-                       
-                       column(4,
-                               numericInput("volume", label = "Volume da amostra", value = 0, min = 1),
-                               numericInput("profundidade_de", label = "Profunidade da coleta", value = 0)),
-                       
-                       column(4,
-                              textInput("referencia_processo_dnpm", label = "Referencia Proocesso ANM"),
-                              actionButton("salvar", label = "Salvar", class = "btn-lg btn-sucess")))
-                     
-                     ),
-
-
-
-
-
-# Fim da Parte Base Hidro/ Amostra água
-###############################################################################   
-       
 
          tabPanel("Assay_Amostra/Dado Geofisico",
                  fluidRow(
                    column(12, div(style = "height: 50px; background-color: gray","Assay_Amostra")),
                    column(3, 
                           fileInput("dataset", label = "Carregar dados", buttonLabel = "upload"),
-                          selectInput("classe", label = "Classe", choices = c("Rocha","Solo","Testemunho","Água"))),
+                          selectInput("classe", label = "Classe", choices = c("Rocha","Solo","Água"))),
                          
-                   column(3, 
-                          textInput("cod_amostra", label = "Código da Amostra"),
-                          numericInput("profundidade_topo", label = "Profundidade do topo", value = 0)),
+                   column(3,
+                          selectInput("tipo-amostra", "Tipo de Amostra", choices = c("Testemunho", "Calha", "etc" )),
+                          textInput("cod_amostra", label = "Código da Amostra")),
                           
                    column(3,
                           numericInput("profundidade_base", label = "Profundidade da base", value = 0 ),
-                          textInput("observacao", label = "Observação")),
+                          numericInput("profundidade_topo", label = "Profundidade do topo", value = 0)),
                          
                    column(3,
-                          selectInput("nome_da_rocha", label = "Nome da Rocha",
-                                      choices = c("Dicionário de nomes de Rochas", "Rocha A", "Rocha B", "Rocha C", "Rocha N....")),
+                          
                           actionButton("salvar", label = "Salvar", class = "btn-lg btn-sucess"),
                           actionButton("modulo_amostra", label = "Modulo Amostra", class = "btn-lg btn-sucess"))
                          
@@ -226,14 +195,21 @@ ui <- fluidPage( theme = bslib::bs_theme(bg = "white",fg = "black",   version = 
                                                  "Gravimetria", "Magnetometria","Eletromagnético",
                                                  "Polarização Induzida","Magnetotelúrico","Resistividade",
                                                  "Potencial espontaneo","Radiométrico"
-                                                 ), multiple = T)),
-                   column(4,
+                                                 ), multiple = T),
                           selectInput("id_equipamento_geofisico", label = "Equipamento Geofísico",
                                       choices = c("Equipamento A", "Equipamento B", "Equipamento C", "Equipamento D"),
-                                      multiple = T)),
+                                      multiple = T),
+                          textInput("referencia_resultado", label = "Relatorios e Midias")),
                    
                    column(4,
-                          textInput("referencia_resultado", label = "Resultado"),
+                          textInput("escala_sensibilidade", "Escala de Sensibilidade"),
+                          textInput("Constante_tempo", "Constante de Tempo"),
+                          textInput("velocidade_registro","Velocidade de Registro")),
+                         
+                   
+                   column(4,
+                          numericInput("profundidade_inicial", "Profundidade Inicial", value = 0),
+                          numericInput("profundidade_final", "Profundidade Final", value = 0),
                           actionButton("salvar", label = "Salvar", class = "btn-lg btn-sucess"))
                  )
             )
